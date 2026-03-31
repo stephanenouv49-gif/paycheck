ff0ffde78ca8b9908d38c0ad0407ffa38834fa1c
stephanenouv49-gif <stephanenouv49@gmail.com>
2026-03-31 14:27:48 +0200

    Create sw.js file for service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+// Service Worker Script
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+});
+
+self.addEventListener('activate', function(event) {
+    // Perform activate steps
+});
+
+self.addEventListener('fetch', function(event) {
+    // Perform fetch steps
+});
